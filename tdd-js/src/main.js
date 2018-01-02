fa427cd4293515040c8cbd4f5837c51c87f9c8b2
Rafael Pradella <rafaelpradella@Mac-mini-de-MAC.local>
2018-01-02 06:37:00 -0200

    ES5 Code tested

--- tdd-js/src/main.js
+++ tdd-js/src/main.js
@@ -1,7 +1,21 @@
 module.exports = {
+    
     sum: function(a,b){
-        if(!a || !b || typeof a !== "number" || typeof b !== "number")
-            return false
-        return a + b;
-    } 
+        return (!a || !b || typeof a !== "number" || typeof b !== "number") ? false : a + b;
+    }, 
+
+    sub: function(a,b){
+        return (!a || !b || typeof a !== "number" || typeof b !== "number") ? false : a - b;
+    },
+
+    mult: function(a,b){
+        return (!a || !b || typeof a !== "number" || typeof b !== "number") ? false : a * b;
+    }, 
+
+    div: function(a,b){
+        if( b === 0 )
+            return "Please, don´t divide by zero. For our safety as human race!"
+
+        return (!a || !b || typeof a !== "number" || typeof b !== "number") ? false : a / b;
+    },
 }
--- tdd-js/tests/main.spec.js
+++ tdd-js/tests/main.spec.js
@@ -14,6 +14,21 @@ describe('Calc', () => {
             expect(Calc.sum).to.exist;
             expect(Calc.sum).to.be.a("function");
         });
+
+        it('should exist method mult', () => {
+            expect(Calc.mult).to.exist;
+            expect(Calc.mult).to.be.a("function");
+        });
+
+        it('should exist method sub', () => {
+            expect(Calc.sub).to.exist;
+            expect(Calc.sub).to.be.a("function");
+        });
+
+        it('should exist method div', () => {
+            expect(Calc.div).to.exist;
+            expect(Calc.div).to.be.a("function");
+        });
     });
 
     describe('Sum method', () => {
@@ -27,5 +42,34 @@ describe('Calc', () => {
             expect(Calc.sum("dummyText", new Array())).to.be.false;
         });
 
+    });
+
+    describe('Sub method', () => {
+
+        it('should sub 6-2 and return 4', () => {
+            expect(Calc.sub(6,2)).to.be.equal(4);
+        });
+
+        it('should sub 10-20 and return -10', () => {
+            expect(Calc.sub(10,20)).to.be.equal(-10);
+        });
+
+    });
+
+    describe('Mult method', () => {
+
+        it('should mult 2x2 and return 4', () => {
+            expect(Calc.mult(2,2)).to.be.equal(4);
+        });
+    });
+
+    describe('Div method', () => {
+        it('should mult 8:2 and return 4', () => {
+            expect(Calc.div(8,2)).to.be.equal(4);
+        });
+
+        it('divide by zero', () => {
+            expect(Calc.div(8,0)).to.be.equal("Please, don´t divide by zero. For our safety as human race!");
+        })
     });
 });
