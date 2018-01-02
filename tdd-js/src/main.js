c89d8fe13e8dafcbc525a88fe8d62d5a6f57fe35
Rafael Pradella <rafaelpradella@Mac-mini-de-MAC.local>
2018-01-02 05:59:31 -0200

    TDD-made sum method

--- tdd-js/src/main.js
+++ tdd-js/src/main.js
@@ -1,11 +1,7 @@
-import { union } from 'ramda';
-import sum, {sub} from "./utils";
-
-import react from 'react';
-import reactDOM from 'react-dom';
-
-const arr1 = [1,1,1,2,2,3,4,5,6,6];
-const arr2 = [5,6,6,6,7,8,8,9,9,10,1];
-const allArrays = union(arr1, arr2);
-
-console.log(allArrays);
+module.exports = {
+    sum: function(a,b){
+        if(!a || !b || typeof a !== "number" || typeof b !== "number")
+            return false
+        return a + b;
+    } 
+}
--- tdd-js/tests/main.spec.js
+++ tdd-js/tests/main.spec.js
@@ -1,6 +1,31 @@
 const expect = require("chai").expect;
+const Calc = require("../src/main.js")
 
 describe('Calc', () => {
 
-   
+    //smoke test
+    describe('Smoke Test', () => {
+
+        it('should exist Calc lib', () => {
+            expect(Calc).to.exist;    
+        });
+
+        it('should exist method sum', () => {
+            expect(Calc.sum).to.exist;
+            expect(Calc.sum).to.be.a("function");
+        });
+    });
+
+    describe('Sum method', () => {
+
+        it('should sum 2+2 and return 4', () => {
+            expect(Calc.sum(2,2)).to.be.equal(4);
+        });
+
+        it('should throw a new error if parameters doesnt exist or isNan', () => {
+            expect(Calc.sum(2)).to.be.false;
+            expect(Calc.sum("dummyText", new Array())).to.be.false;
+        });
+
+    });
 });
