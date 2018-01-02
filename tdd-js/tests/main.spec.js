af3c6ba31a162ae834923f2e7e3cc089d944d402
Rafael Pradella <rafaelpradella@Mac-mini-de-MAC.local>
2018-01-02 05:30:37 -0200

    Boilerplating test for Calc project

--- tdd-js/tests/main.spec.js
+++ tdd-js/tests/main.spec.js
@@ -1,25 +1,6 @@
 const expect = require("chai").expect;
 
-describe('Main', () => {
+describe('Calc', () => {
 
-    let arr;
-
-    beforeEach(() => {
-        arr = ["a","b","c"];
-    });
-
-    it('should have length of 4 when pushed another value', () => {
-        arr.push("z");
-        expect(arr).to.have.lengthOf(4);
-    });
-
-    it('should have length of 2 when pop the array', () => {      
-        arr.pop();
-        expect(arr).to.have.lengthOf(2);
-    });
-
-    it('should remove "c" when pop the array', () => {
-        arr.pop();
-        expect(arr).to.not.include("c");
-    });
+   
 });
