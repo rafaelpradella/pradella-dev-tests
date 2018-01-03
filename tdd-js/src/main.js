9921b44a3be8c7056ff6b4e6e513ee02b71aedec
Rafael Pradella <pradella@outlook.com>
2018-01-03 13:03:23 -0200

    Bring all Calc functions to a single const

--- tdd-js/src/main.js
+++ tdd-js/src/main.js
@@ -1,21 +1,30 @@
-module.exports = {
+const Calc = {
+    
+    isValidParameters: function(a,b){
+        console.log(typeof a);
+        console.log(typeof b);
+        return (a && b && typeof a === "number" && typeof b === "number") ? true : false;
+    },
     
     sum: function(a,b){
-        return (!a || !b || typeof a !== "number" || typeof b !== "number") ? false : a + b;
+        return (this.isValidParameters(a,b)) ? a + b : false;
     }, 
 
     sub: function(a,b){
-        return (!a || !b || typeof a !== "number" || typeof b !== "number") ? false : a - b;
+        return (this.isValidParameters(a,b)) ? a - b : false;
     },
 
     mult: function(a,b){
-        return (!a || !b || typeof a !== "number" || typeof b !== "number") ? false : a * b;
+        return (this.isValidParameters(a,b)) ? a * b : false;
     }, 
 
     div: function(a,b){
-        if( b === 0 )
-            return "Please, don´t divide by zero. For our safety as human race!"
-
-        return (!a || !b || typeof a !== "number" || typeof b !== "number") ? false : a / b;
-    },
+        if( b === 0 ) return "Please, don´t divide by zero. For our safety as human race!"
+        return (this.isValidParameters(a,b)) ? a / b : false;
+    }
 }
+
+console.log(Calc.sum(1,"Capybara"));
+console.log(Calc.sum(1));
+
+module.exports = Calc;
