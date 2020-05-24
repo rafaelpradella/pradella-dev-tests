b72ecd60c42d0bd65702520d25dbcbaa3dd98704
Rafael Pradella <pradella@outlook.com>
2020-05-24 02:21:30 -0300

    dash to slash for better browser compat

--- alura-typescript/sample-app/js/controllers/transactionController.js
+++ alura-typescript/sample-app/js/controllers/transactionController.js
@@ -6,7 +6,7 @@ class TransactionController {
     }
     add(event) {
         event.preventDefault();
-        const transaction = new Transaction(new Date(this.inputData.value.replace(/-/g, ",")), parseInt(this.inputQuantity.value), parseFloat(this.inputValue.value));
+        const transaction = new Transaction(new Date(this.inputData.value.replace(/-/g, "/")), parseInt(this.inputQuantity.value), parseFloat(this.inputValue.value));
         console.log(transaction);
     }
 }
